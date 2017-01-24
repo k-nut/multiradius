860579120f5e5aefd2079e837bb2da7a810b3939
Knut Hühne <knut@k-nut.eu>
2017-01-24 19:27:17 +0100

    initial commit

---
+++ Control.OSMGeocoder.js
@@ -0,0 +1,278 @@
+/*
+L.Control.OSMGeocoder = L.Control.extend({
+	options: {
+		collapsed: false,
+		position: 'topright',
+		text: 'Ort suchen',
+		bounds: new L.LatLngBounds([52.3380737304688, 13.0883140563965], [52.6754760742188, 13.7609090805054]), // L.LatLngBounds
+		//bounds: new L.LatLngBounds([52.483642578125, 13.368328094482], [52.531131744384, 13.4914817810059]), // L.LatLngBounds
+		email: null, // String
+		callback: function (results) {
+			//console.log(results);
+			var bbox = results[0].boundingbox,
+			first = new L.LatLng(bbox[0], bbox[2]),
+			second = new L.LatLng(bbox[1], bbox[3]),
+			bounds = new L.LatLngBounds([first, second]);
+			this._map.fitBounds(bounds);
+
+			center = new L.LatLng(results[0].lat, results[0].lon);
+			addMarkerWithRadius(center, R);
+		}
+	},
+
+	_callbackId: 0,
+
+	initialize: function (options) {
+		L.Util.setOptions(this, options);
+	},
+
+	onAdd: function (map) {
+		this._map = map;
+		var className = 'leaflet-control-geocoder',
+		container = this._container = L.DomUtil.create('div', className);
+
+		L.DomEvent.disableClickPropagation(container);
+
+		var form = this._form = L.DomUtil.create('form', className + '-form');
+
+		var input = this._input = document.createElement('input');
+		input.type = "text";
+
+		var submit = document.createElement('button');
+		submit.type = "submit";
+		submit.innerHTML = this.options.text;
+
+		form.appendChild(input);
+		form.appendChild(submit);
+
+		L.DomEvent.addListener(form, 'submit', this._geocode, this);
+
+		if (this.options.collapsed) {
+			L.DomEvent.addListener(container, 'mouseover', this._expand, this);
+			L.DomEvent.addListener(container, 'mouseout', this._collapse, this);
+
+			var link = this._layersLink = L.DomUtil.create('a', className + '-toggle', container);
+			link.href = '#';
+			link.title = 'Nominatim Geocoder';
+
+			L.DomEvent.addListener(link, L.Browser.touch ? 'click' : 'focus', this._expand, this);
+
+			this._map.on('movestart', this._collapse, this);
+		} else {
+			this._expand();
+		}
+
+		container.appendChild(form);
+
+		var rform = this._rform = L.DomUtil.create('form', className + '-rform');
+
+		var rsubmit = document.createElement('span');
+		rsubmit.innerHTML = "Meter Radius";
+		//var rinput = this._rinput = document.createElement('input');
+		//rinput.type = "number";
+		//rinput.step = 100;
+		rinput.value = R;
+
+		L.DomEvent.addListener(rinput, 'input', this._test, this);
+
+
+		rform.appendChild(rinput);
+		rform.appendChild(rsubmit);
+
+		container.appendChild(rform);
+
+		return container;
+	},
+
+	_test : function (event){
+		L.DomEvent.preventDefault(event); 
+		R = parseInt(this._rinput.value, 10);
+		addMarkerWithRadius(center, R);
+	},
+
+	_geocode : function (event) {
+		L.DomEvent.preventDefault(event);
+		//http://wiki.openstreetmap.org/wiki/Nominatim
+		this._callbackId = "_l_osmgeocoder_" + (this._callbackId++);
+		window[this._callbackId] = L.Util.bind(this.options.callback, this);
+
+
+		// Set up params to send to Nominatim 
+		var params = {
+			// Defaults
+			q: this._input.value,
+			json_callback : this._callbackId,
+			format: 'json'
+		};
+
+		if (this.options.bounds && this.options.bounds != null) {
+			if( this.options.bounds instanceof L.LatLngBounds ) {
+				params.viewbox = this.options.bounds.toBBoxString();
+				params.bounded = 1;
+			}
+			else {
+				//console.log('bounds must be of type L.LatLngBounds');
+				return;
+			}
+		}
+
+		if (this.options.email && this.options.email != null) {
+			if (typeof this.options.email == 'string') {
+				params.email = this.options.email;
+			}
+			else{
+				//console.log('email must be a string');
+			}
+		}
+
+		var url = " http://nominatim.openstreetmap.org/search" + L.Util.getParamString(params),
+		script = document.createElement("script");
+
+
+
+
+		script.type = "text/javascript";
+		script.src = url;
+		script.id = this._callbackId;
+		document.getElementsByTagName("head")[0].appendChild(script);
+	},
+
+	_expand: function () {
+		L.DomUtil.addClass(this._container, 'leaflet-control-geocoder-expanded');
+	},
+
+	_collapse: function () {
+		//this._container.className = this._container.className.replace(' leaflet-control-geocoder-expanded', '');
+	}
+});
+
+
+*/
+
+
+if (typeof console == "undefined") {
+	this.console = { log: function (msg) { /* do nothing since it would otherwise break IE */} };
+}
+
+
+L.Control.OSMGeocoder = L.Control.extend({
+	options: {
+		collapsed: true,
+		position: 'topright',
+		text: 'Locate',
+		bounds: null, // L.LatLngBounds
+		email: null, // String
+		callback: function (results) {
+			console.log(results);
+			var bbox = results[0].boundingbox,
+			first = new L.LatLng(bbox[0], bbox[2]),
+			second = new L.LatLng(bbox[1], bbox[3]),
+			bounds = new L.LatLngBounds([first, second]);
+			this._map.fitBounds(bounds);
+		}
+	},
+
+	_callbackId: 0,
+
+	initialize: function (options) {
+		L.Util.setOptions(this, options);
+	},
+
+	onAdd: function (map) {
+		this._map = map;
+		var className = 'leaflet-control-geocoder',
+		container = this._container = L.DomUtil.create('div', className);
+
+		L.DomEvent.disableClickPropagation(container);
+
+		var form = this._form = L.DomUtil.create('form', className + '-form');
+
+		var input = this._input = document.createElement('input');
+		input.type = "text";
+
+		var submit = document.createElement('input');
+		submit.type = "submit";
+		submit.value = this.options.text;
+
+		form.appendChild(input);
+		form.appendChild(submit);
+
+		L.DomEvent.addListener(form, 'submit', this._geocode, this);
+
+		if (this.options.collapsed) {
+			L.DomEvent.addListener(container, 'mouseover', this._expand, this);
+			L.DomEvent.addListener(container, 'mouseout', this._collapse, this);
+
+			var link = this._layersLink = L.DomUtil.create('a', className + '-toggle', container);
+			link.href = '#';
+			link.title = 'Nominatim Geocoder';
+
+			L.DomEvent.addListener(link, L.Browser.touch ? 'click' : 'focus', this._expand, this);
+
+			this._map.on('movestart', this._collapse, this);
+		} else {
+			this._expand();
+		}
+
+		container.appendChild(form);
+		return container;
+	},
+
+	_geocode : function (event) {
+		L.DomEvent.preventDefault(event);
+		//http://wiki.openstreetmap.org/wiki/Nominatim
+		this._callbackId = "_l_osmgeocoder_" + (this._callbackId++);
+		window[this._callbackId] = L.Util.bind(this.options.callback, this);
+
+
+		/* Set up params to send to Nominatim */
+		var params = {
+			// Defaults
+			q: this._input.value,
+			json_callback : this._callbackId,
+			format: 'json'
+		};
+
+		if (this.options.bounds && this.options.bounds != null) {
+			if( this.options.bounds instanceof L.LatLngBounds ) {
+				params.viewbox = this.options.bounds.toBBoxString();
+				params.bounded = 1;
+			}
+			else {
+				console.log('bounds must be of type L.LatLngBounds');
+				return;
+			}
+		}
+
+		if (this.options.email && this.options.email != null) {
+			if (typeof this.options.email == 'string') {
+				params.email = this.options.email;
+			}
+			else{
+				console.log('email must be a string');
+			}
+		}
+
+		var url = " http://nominatim.openstreetmap.org/search" + L.Util.getParamString(params),
+		script = document.createElement("script");
+
+
+
+
+		script.type = "text/javascript";
+		script.src = url;
+		script.id = this._callbackId;
+		document.getElementsByTagName("head")[0].appendChild(script);
+	},
+
+	_expand: function () {
+		L.DomUtil.addClass(this._container, 'leaflet-control-geocoder-expanded');
+	},
+
+	_collapse: function () {
+		this._container.className = this._container.className.replace(' leaflet-control-geocoder-expanded', '');
+	}
+});
+
+
+
---
+++ main.js
@@ -0,0 +1,155 @@
+var circles = null;
+var radiuses = [100, 200, 300];
+var lastCoordinates = [];
+
+function addMarkerWithRadius(latlon){
+  //TODO: refactor this to remove global variables
+  if (typeof(circles) !== "undefined"  && typeof(marker) !== "undefined"){
+    map.removeLayer(circles);
+    map.removeLayer(marker);
+  }
+
+  lastCoordinates = latlon;
+
+  marker = L.marker(latlon, {draggable: true, icon: L.icon({iconUrl: "green-marker.png", iconSize: [25, 41],
+      iconAnchor: [12.5, 41]})}).addTo(map);
+  marker.on("dragend", function(e){
+    X = e.target._latlng;
+    latlon = new L.LatLng(X.lat, X.lng);
+    addMarkerWithRadius(latlon);
+    $(".leaflet-control-geocoder-form").children().first().val('');
+    updatePrintHeader();
+  });
+
+  var colors = ['green', 'blue', 'red'];
+  circles = L.layerGroup();
+  radiuses = [$("#radius").val(), $("#radius2").val(), $("#radius3").val()]
+  radiuses.forEach(function(radius, index){
+    var circle = L.circle(latlon, {
+      radius: radius,
+      color: colors[index],
+      fillColor: colors[index],
+      fillOpacity: 0.0
+    }).addTo(map);
+    circle.addTo(circles);
+    circle.bringToBack();
+  });
+  circles.addTo(map);
+
+}
+
+var cloudmadeAttribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+var cloudmade = new L.TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: cloudmadeAttribution
+});
+
+var map = new L.Map('map').addLayer(cloudmade).setView(new L.LatLng(52.52, 13.35), 11);
+
+var berlinBounds = new L.LatLngBounds([52.3380737304688, 13.0883140563965], [52.6754760742188, 13.7609090805054]);
+var osmGeocoder = new L.Control.OSMGeocoder(
+  {collapsed: false,
+    text:"Ort suchen",
+    bounds: berlinBounds, 
+    callback: function (results) {
+      var bbox = results[0].boundingbox,
+      first = new L.LatLng(bbox[0], bbox[2]),
+      second = new L.LatLng(bbox[1], bbox[3]),
+      bounds = new L.LatLngBounds([first, second]);
+
+      var center = new L.LatLng(results[0].lat, results[0].lon);
+      var latlon = new L.LatLng(results[0].lat, results[0].lon);
+      addMarkerWithRadius(latlon);
+    }
+  } 
+);
+map.addControl(osmGeocoder);
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var data = JSON.parse(this.response);
+    L.geoJSON(data, {
+      style:  {
+        "color": "black",
+        "weight": 2,
+        "opacity": 0.65,
+        "fillOpacity": 0
+      }}).addTo(map);
+  }
+};
+xhttp.open("GET", "berlin_bezirke.geojson", true);
+xhttp.send();
+
+
+function update_r(){
+  addMarkerWithRadius(lastCoordinates);
+  updatePrintHeader();
+}
+
+function set_print(){
+  var m = $('#map');
+  var mapWidth = $('#map').width();
+  var mapHeight = $('#map').height();
+  var ratio = mapWidth / mapHeight;
+  var A4Width = '18cm';
+  var A4Height = '20cm';
+  var checked = $('#print_enabled').is(':checked');
+  if (checked){
+    if (ratio < 1){
+      m.width(A4Width);
+      m.height(A4Height);
+    }
+    else {
+      m.width(A4Height);
+      m.height(A4Width);
+    }
+  }
+  else {
+    m.width("100%");
+    m.height("100%");
+  }
+  if (typeof(marker) !== "undefined"){
+    L.Util.requestAnimFrame(map.invalidateSize,map,!1,map._container);
+    map.panTo(marker._latlng);
+  }
+  $('#map').css('display', 'none').css('display', 'block');
+  L.Util.requestAnimFrame(map.invalidateSize,map,!1,map._container);
+  map.invalidateSize(false); // TODO check if this is still needed
+}
+
+
+function updatePrintHeader(){
+  var searchInput = $(".leaflet-control-geocoder-form").children().first().val();
+  $("#print-header").text(searchInput + '| Radius: ' +  $("#radius").val() + '/' +  $("#radius2").val() + '/' + $("#radius3").val()+ ' Meter');
+}
+
+function emptyIt(){
+  $("#radius").val("100");
+  $("#print_enabled").attr("checked", false);
+  $(".leaflet-control-geocoder-form").children().first().val("");
+}
+
+
+var radius_setter = $("<div class='leaflet-control-geocoder-form'>");
+radius_setter.append("<input type='text' id=radius name='radius' value=" + radiuses[0] + " onKeyUp='update_r()'>");
+radius_setter.append("<span> Meter Radius </span>"); // TODO align
+
+radius_setter.append("<input type='text' id=radius2 name='radius' value=" + radiuses[1] + " onKeyUp='update_r()'>");
+radius_setter.append("<span> Meter Radius </span>"); // TODO align
+
+radius_setter.append("<input type='text' id=radius3 name='radius' value=" + radiuses[2] + " onKeyUp='update_r()'>");
+radius_setter.append("<span> Meter Radius </span>"); // TODO align
+
+var print_enabler = $("<div class='leaflet-control-geocoder-form'>");
+print_enabler.append("<input type='checkbox' id='print_enabled'>");
+print_enabler.append("<label for='print_enabled'> Auf Druckgröße verkleinern </label>"); 
+
+$(".leaflet-control-geocoder").append(radius_setter);
+$(".leaflet-control-geocoder").append(print_enabler);
+
+$("#print_enabled").click(function() { set_print();});
+
+$(".leaflet-control-geocoder-form").children().first().change(function() { updatePrintHeader();});
+
+updatePrintHeader();
+window.setTimeout(function(){emptyIt();}, 400);
